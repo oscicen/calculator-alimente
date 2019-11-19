979e0c556ae7d853c7e4db6e1e9a48837fc371ab
Andrei Lusca <alusca@intralinks.com>
2019-11-19 18:21:51 +0200

    Updates

---
+++ precache-manifest.c7456f6cf2ac5d603277b453af72b531.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7597e5eaa187ededf2c3e2d5fba8463",
+    "url": "/calculator-alimente/index.html"
+  },
+  {
+    "revision": "b46b5aa1c4b05d3956fb",
+    "url": "/calculator-alimente/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "35124a7a4eb0ba5a9396",
+    "url": "/calculator-alimente/static/js/2.c14d3534.chunk.js"
+  },
+  {
+    "revision": "b46b5aa1c4b05d3956fb",
+    "url": "/calculator-alimente/static/js/main.689d6d96.chunk.js"
+  },
+  {
+    "revision": "2c6247682e27ae58455b",
+    "url": "/calculator-alimente/static/js/runtime-main.74a76af8.js"
+  }
+]);
